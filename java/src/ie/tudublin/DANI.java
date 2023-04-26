80f8eadcfda98802ed92d74c431c9d6b85e3c6c8
Adrian T Capacite <45900436+AdrianCapacite@users.noreply.github.com>
2023-04-26 16:41:45 +0100

    Setup DANI skeleton

--- java/src/ie/tudublin/DANI.java
+++ java/src/ie/tudublin/DANI.java
@@ -6,39 +6,82 @@ import processing.core.PApplet;
 
 public class DANI extends PApplet {
 
-	
+	String[] sonnet;
+	float off = 0;
 
+	// Processing
 	public void settings() {
 		size(1000, 1000);
-		//fullScreen(SPAN);
+		// fullScreen(SPAN);
 	}
-
-    String[] sonnet;
-
-    public String[] writeSonnet()
-    {
-        return null;
-    }
 
 	public void setup() {
 		colorMode(HSB);
 
-       
 	}
 
 	public void keyPressed() {
-
+		// TODO: Generate space to create new sonnet
 	}
 
-	float off = 0;
-
-	public void draw() 
-    {
+	public void draw() {
 		background(0);
 		fill(255);
 		noStroke();
 		textSize(20);
-        textAlign(CENTER, CENTER);
-        
+		textAlign(CENTER, CENTER);
+
+	}
+
+	// Dani
+	/**
+	 * Function loads a file into DANI's model
+	 * @param filename
+	 */
+	public void loadFile(String filename) {
+		// loadStrings("filename.txt"); // Load a text file into a String array
+		// split(line, ' '); // Split a string into an array of words
+		// w.replaceAll("[^\\w\\s]",""); // Remove punction characters
+		// s.toLowerCase() // Convert a string to lower case
+	}
+
+	/**
+	 * Finds a word in the model
+	 * @param word
+	 */
+	public void findWord(String word) {
+
+	}
+
+	// TODO: Add findFollow to word class
+
+	/**
+	 * Displays model as list of words with their follow
+	 * Displayed as:
+	 * 		i: love(2)
+	 * 		love: star(1) is(1) tu(1)
+	 * 		star: trek(1)
+	 * 		trek:
+	 * 		is: love(1)
+	 * 		tu: dublin(1)
+	 * 		dublin:
+	 */
+	public void printModel() {
+		// TODO: Implement
+	}
+
+	/**
+	 * Returns a 14 line sonnet
+	 * @return
+	 */
+	public String[] writeSonnet() {
+		return null;
+	}
+
+	/**
+	 * Calls writeSonnet then displays it to screen and console
+	 */
+	public void createSonnet() {
+
 	}
 }
